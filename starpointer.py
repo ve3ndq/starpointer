267efc68eafa547cd1ac6524ff6ab7347e2f72c5
CanEngineer <nathandykstra@Nathans-MBP.lan>
2019-08-11 16:45:43 -0400

    a

--- starpointer.py
+++ starpointer.py
@@ -2,58 +2,70 @@ import sys
 #sys.path.append("/usr/local/lib/python3.6/site-packages/skyfield")
 print(sys.path)
 from skyfield.api import Topos, load
+import time
+
 stations_url = 'http://celestrak.com/NORAD/elements/stations.txt'
-satellites = load.tle(stations_url)
-satellite = satellites['ISS (ZARYA)']
 
-print('-------------------SATELLITE:')
-print(satellite)
-
-print('EPOCH')
-print(satellite.epoch.utc_jpl())
-
-
-ts = load.timescale()
-#t = ts.utc(2014, 1, 23, 11, 18, 7)
-t=ts.now()
-
-days = t - satellite.epoch
-print('{:.3f} days away from epoch'.format(days))
-#TESTING 123
-if abs(days) > 7:
-    satellites = load.tle(stations_url, reload=True)
+while (1==1):
+    satellites = load.tle(stations_url)
     satellite = satellites['ISS (ZARYA)']
 
-geocentric = satellite.at(t)
-print('GEOCENTRIC POS:')
-print(geocentric.position.km)
+    print('-------------------SATELLITE:')
+    print(satellite)
 
-bluffton = Topos('40.8939 N', '83.8917 W')
-burlington = Topos('43.328674 N', '79.817734 W')
+    print('EPOCH')
+    print(satellite.epoch.utc_jpl())
 
-difference = satellite - burlington
-print('DIFFERENCE:')
-print(difference)
 
-topocentric = difference.at(t)
-print('TOPOCENTRIC POSITION:')
-print(topocentric.position.km)
+    ts = load.timescale()
+    #t = ts.utc(2014, 1, 23, 11, 18, 7)
+    t=ts.now()
 
-alt, az, distance = topocentric.altaz()
+    days = t - satellite.epoch
+    print('{:.3f} days away from epoch'.format(days))
+    #TESTING 123
+    if abs(days) > 7:
+        satellites = load.tle(stations_url, reload=True)
+        satellite = satellites['ISS (ZARYA)']
 
-if alt.degrees > 0:
-    print('The ISS is above the horizon')
-print("ALT/AZ/DIST:")
-print(alt)
-print(az)
-print(distance.km)
-ra, dec, distance = topocentric.radec()  # ICRF ("J2000")
+    geocentric = satellite.at(t)
 
-subpoint = geocentric.subpoint()
-print('Latitude:', subpoint.latitude)
-print('Longitude:', subpoint.longitude)
-print('Elevation (m):', int(subpoint.elevation.m))
 
+    print('GEOCENTRIC POS:')
+    print(geocentric.position.km)
+
+    #bluffton = Topos('40.8939 N', '83.8917 W')
+    burlington = Topos('43.328674 N', '79.817734 W')
+
+    difference = satellite - burlington
+    print('DIFFERENCE:')
+    print(difference)
+
+    topocentric = difference.at(t)
+    print('TOPOCENTRIC POSITION:')
+    print(topocentric.position.km)
+
+
+    #if alt.degrees > 0:
+    #    print('The ISS is above the horizon')
+    alt, az, distance = topocentric.altaz()
+
+
+    print("ALT/AZ/DIST:")
+    print(alt)
+    print(alt.degrees)
+    print('Az:')
+    print(az)
+    print(az.degrees)
+    print('Distance:')
+    print(distance.km)
+    ra, dec, distance = topocentric.radec()  # ICRF ("J2000")
+
+    subpoint = geocentric.subpoint()
+    print('Latitude:', subpoint.latitude)
+    print('Longitude:', subpoint.longitude)
+    print('Elevation (m):', int(subpoint.elevation.m))
+    time.sleep(3)
 
 #print("RA/DEC:")
 #print(ra)
